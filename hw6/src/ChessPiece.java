fda50709b233d9a905714532536d0fd685ed87a5
WEN TING LIN <wentinglin@WENs-MacBook-Pro.local>
2021-02-27 15:09:29 -0800

    hw6

---
+++ hw6/src/ChessPiece.java
@@ -0,0 +1,41 @@
+/**
+ * This interface represents chess piece.
+ */
+public interface ChessPiece {
+    int MAXROW = 8;
+    int MAXCOlUMN = 8;
+
+    /**
+     * Determine current position
+     * @return row
+     */
+    public int getRow();
+
+    /**
+     * Determine current position
+     * @return column
+     */
+    public int getColumn();
+
+    /**
+     * Determine color
+     * @return color
+     */
+    public ChessColor getColor();
+
+    /**
+     * Determine if the chess can move to certain position
+     * @param row row
+     * @param col column
+     * @return true if can move
+     */
+    public boolean canMove(int row, int col);
+
+    /**
+     * Determine if the chess can kill the chess input
+     * @param piece the ChessPiece object
+     * @return boolean
+     */
+    public boolean canKill(ChessPiece piece);
+
+}
